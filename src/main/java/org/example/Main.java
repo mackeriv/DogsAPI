04fea57c760e30731f70d11e8cd7f8f02d098d6f
mackeriv <mackeriv@gmail.com>
2023-07-06 14:12:38 -0300

    Random fixes.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,35 +6,44 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.http.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
 
         URI uri = new URI("https://api.thedogapi.com/v1/images/search?limit=10");
         var client = java.net.http.HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(uri).GET().build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException ioex1) {
+            ioex1.printStackTrace();
+        }
 
         var parser = new JsonParser();
-
         var parsed = parser.parse(response.body());
+
+        var dir = new File("output/");
+        dir.mkdir();
 
         for(var items : parsed) {
 
             String ID = items.get("id");
             URI imageAddress = new URI(items.get("url"));
 
-            System.out.println("ID: " + ID);
-            System.out.println("URL: " + imageAddress);
+            System.out.println("\033[0;1m" + "ID: " + "\033[0;m" + ID);
+            System.out.println("\033[0;1m" + "URL: " + "\033[0;m" + imageAddress);
 
-            BufferedImage image = ImageIO.read(imageAddress.toURL());
+            try {
+                BufferedImage image = ImageIO.read(imageAddress.toURL());
+                ImageIO.write(image, "jpg", new File("output/" + ID + imageAddress.toString().substring(imageAddress.toString().length() - 4)));
+            } catch (IOException ioex2) {
+                ioex2.printStackTrace();
+            }
 
-            ImageIO.write(image, "jpg", new File("C:\\Users\\rafa_\\Desktop\\Java\\Java 8 - Beyond the Basics\\Dogs\\output\\" + ID + imageAddress.toString().substring(imageAddress.toString().length()-4)));
-
-            System.out.println("Image saved!");
+            System.out.println("\033[0;1m" + "Image saved!");
             System.out.println("\n");
 
 
